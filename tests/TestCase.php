6e37db1f8b0214d26403f547d111aec53dd1c4b0
ihsanfawzan <ihsanfawzan@users.noreply.github.com>
2023-06-20 08:23:55 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Response\Response\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Response\Response\ResponseServiceProvider;
 
